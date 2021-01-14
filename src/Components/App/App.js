67250c3658c3d21e94eadc8d9c47ee88f071f438
Scott Brabson <scottbrabson@gmail.com>
2021-01-14 13:22:36 -0700

    separate hook into function for prop drilling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,9 +6,14 @@ import FindSpotView from "../FindSpotsView/FindSpotView";
 import SpotDetails from '../SpotDetails/SpotDetails'
 import {skateSpots} from '../../sk8SpotData'
 import NavBar from '../Common/NavBar/NavBar'
+
 function App() {
 
   const [selectedSpot, setSelectedSpot] = useState(null)
+
+  const updateSelection = (spot) => {
+    setSelectedSpot(spot)
+  }
 
   return (
     <div className="App">
@@ -16,10 +21,10 @@ function App() {
         <NavBar/>
         <Switch>
           <Route path="/spots/:spot_id">
-            <SpotDetails selectedSpot={selectedSpot} />
+            <SpotDetails updateSelection={updateSelection} />
           </Route>
           <Route path="/search">
-            <FindSpotView setSelectedSpot={setSelectedSpot} skateSpots={skateSpots} />
+            <FindSpotView updateSelection={updateSelection} skateSpots={skateSpots} />
           </Route>
           <Route path="/add">
             <AddSpotView skateSpots={skateSpots} />
