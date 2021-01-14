e9a34111594abb9d21ccd73bec3a84aecb7f48ba
Scott Brabson <scottbrabson@gmail.com>
2021-01-14 13:22:08 -0700

    Add functionality to pan and zoom to clicked marker

--- src/Components/Common/Map/Map.jsx
+++ src/Components/Common/Map/Map.jsx
@@ -1,27 +1,20 @@
 import React, { useState, useCallback, useEffect } from "react";
 import "./Map.css";
-import {
-  GoogleMap,
-  useJsApiLoader,
-  Marker
-} from "@react-google-maps/api";
-import SpotInfoBox from "../SpotInfoBox/SpotInfoBox";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import SpotMarker from "./SpotMarker/SpotMarker";
+import SpotInfoBox from "./SpotInfoBox/SpotInfoBox";
 
 const containerStyle = {
   width: "1000px",
   height: "600px",
 };
 
-let position = {
+let defaultPosition = {
   lat: 39.816,
   lng: -105.065,
 };
 
-const addMarker = latLng => {
-  position = latLng;
-};
-
-const Map = ({ skateSpots, setSelectedSpot }) => {
+const Map = ({ skateSpots, updateSelection }) => {
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyCjoD7gA6zlBj9bDgWw0ug2LpbMvu9ypB0",
@@ -29,25 +22,40 @@ const Map = ({ skateSpots, setSelectedSpot }) => {
 
   const [map, setMap] = useState(null);
   const [selectedMarker, setSelectedMarker] = useState(null);
+  const [center, setCenter] = useState(defaultPosition);
+  const [zoom, setZoom] = useState(12);
+
+  const recenter = latLng => {
+    setCenter(latLng);
+  };
 
   const onLoad = useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
     map.fitBounds(bounds);
+    setCenter(defaultPosition);
     setMap(map);
+    map.panTo(defaultPosition);
   }, []);
 
   const onUnmount = useCallback(function callback(map) {
     setMap(null);
   }, []);
 
+  const handleMarkerClick = (e, spot) => {
+    setSelectedMarker(spot);
+    setZoom(14)
+    setCenter(defaultPosition);
+    map.panTo(defaultPosition);    
+    updateSelection(spot);
+  }
+
   const markers = skateSpots?.map((spot, i) => (
     <Marker
       key={Date.now() + i}
       position={spot.location}
       label={spot.title}
-      onClick={() => {
-        setSelectedMarker(spot);
-        setSelectedSpot(spot);
+      onClick={(e) => {
+        handleMarkerClick(e, spot)
       }}
     />
   ));
@@ -57,19 +65,26 @@ const Map = ({ skateSpots, setSelectedSpot }) => {
       <div className="map-container">
         <GoogleMap
           mapContainerStyle={containerStyle}
-          defaultCenter={{ lat: -34.397, lng: 150.644 }}
-          center={position}
-          zoom={1}
+          center={center}
+          zoom={zoom}
           onLoad={onLoad}
           onUnmount={onUnmount}
-          onClick={e => addMarker(e.latLng)}>
+          // onClick={e => recenter(e.latLng)}
+        >
           {/* Child components, such as markers, info windows, etc. */}
           {markers}
+          {/* {skateSpots && (
+            <SpotMarker
+              setSelectedMarker={setSelectedMarker}
+              updateSelection={updateSelection}
+              skateSpots={skateSpots}
+            />
+          )} */}
           {selectedMarker && (
             <SpotInfoBox
               selectedMarker={selectedMarker}
               setSelectedMarker={setSelectedMarker}
-              setSelectedSpot={setSelectedSpot}
+              updateSelection={updateSelection}
             />
           )}
         </GoogleMap>
@@ -78,7 +93,11 @@ const Map = ({ skateSpots, setSelectedSpot }) => {
   };
 
   if (loadError) {
-    return <div>Can't find the map right now. I'll keep looking, come back later...</div>;
+    return (
+      <div>
+        Can't find the map right now. I'll keep looking, come back later...
+      </div>
+    );
   }
   return isLoaded ? renderMap() : <h1>Now where did I put that map...</h1>;
 };
