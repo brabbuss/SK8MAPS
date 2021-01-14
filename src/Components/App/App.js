242d446cd9222226a64f5c3b04c34f9d25389b5d
Scott Brabson <scottbrabson@gmail.com>
2021-01-13 21:06:28 -0700

    implement switch and route/endpoints

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,43 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import AddSpotView from "../AddSpotView/AddSpotView";
 import FindSpotView from "../FindSpotsView/FindSpotView";
+import {sk8Logo} from '../Common/logo'
 
 const skateSpots = [
   {
-    title: 'Secrest Park', 
+    title: "Secrest Park",
     location: {
       lat: 39.8155,
-      lng:  -105.0726
-    }
+      lng: -105.0726,
+    },
   },
   {
-    title: "Scott's driveway", 
+    title: "Scott's driveway",
     location: {
       lat: 39.816,
-      lng: -105.065
-    }
+      lng: -105.065,
+    },
   },
   {
-    title: "Arvada SkatePark", 
+    title: "Arvada SkatePark",
     location: {
-      lat: 39.82663239233444, 
-      lng: -105.14309411715244
-    }
-  }
-]
+      lat: 39.82663239233444,
+      lng: -105.14309411715244,
+    },
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <FindSpotView
-          skateSpots={skateSpots}
-        />
-        <AddSpotView
-          skateSpots={skateSpots}
-        />
+        <div className='app-title'>
+          {sk8Logo}
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <Switch>
+          <Route path="/find">
+            <FindSpotView skateSpots={skateSpots} />
+          </Route>
+          <Route path="/add">
+            <AddSpotView skateSpots={skateSpots} />
+          </Route>
+        </Switch>
       </header>
     </div>
   );
