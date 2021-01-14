94867b41a25ec3026fd3cd2af21c418c839f1fbf
Scott Brabson <scottbrabson@gmail.com>
2021-01-14 13:22:55 -0700

    add structure for highlighted nav links

--- src/Components/Common/NavBar/NavBar.jsx
+++ src/Components/Common/NavBar/NavBar.jsx
@@ -3,17 +3,23 @@ import "./NavBar.css";
 import { sk8Logo } from "../sk8logo";
 
 const NavBar = () => {
+  const highlight = (e) => {
+    // if (!e.className.includes(`${e.target}-highlight`)) {
+    //   e.className = `nav-btn ${e.target}-highlight` 
+    // } 
+  }
+  
   return (
     <header className="nav-container">
-      <Link to="/" className="app-name-link">
+      <Link to="/" onClick={(e) => highlight(e)} value='nav-title' className="app-name-link">
         {sk8Logo}
       </Link>
       <nav className="link-container">
         <Link to="/search" className="nav-btn-link">
-          <div className="nav-btn">Search</div>
+          <div onClick={(e) => highlight(e)} value='search' className="nav-btn">Search</div>
         </Link>
         <Link to="/add" className="nav-btn-link">
-          <div className="nav-btn">Contribute</div>
+          <div onClick={(e) => highlight(e)} value='contribute' className="nav-btn">Contribute</div>
         </Link>
       </nav>
     </header>
