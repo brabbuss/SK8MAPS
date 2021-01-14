3cc8c823a1c386374e331bb6a5701928cd9c05d0
Scott Brabson <scottbrabson@gmail.com>
2021-01-13 21:29:54 -0700

    migrate app data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,30 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import AddSpotView from "../AddSpotView/AddSpotView";
 import FindSpotView from "../FindSpotsView/FindSpotView";
 import {sk8Logo} from '../Common/logo'
-
-const skateSpots = [
-  {
-    title: "Secrest Park",
-    location: {
-      lat: 39.8155,
-      lng: -105.0726,
-    },
-  },
-  {
-    title: "Scott's driveway",
-    location: {
-      lat: 39.816,
-      lng: -105.065,
-    },
-  },
-  {
-    title: "Arvada SkatePark",
-    location: {
-      lat: 39.82663239233444,
-      lng: -105.14309411715244,
-    },
-  },
-];
+import {skateSpots} from '../../sk8SpotData'
 
 function App() {
   return (
