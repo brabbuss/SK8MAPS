8ac76a33e37e1aad101d27bd1bea819d519e6113
Scott Brabson <scottbrabson@gmail.com>
2021-01-14 16:21:06 -0700

    update sample data

--- src/sk8SpotData.js
+++ src/sk8SpotData.js
@@ -20,25 +20,55 @@ export const skateSpots = [
   {
     id: 2,
     title: "Secrest Park",
+    description: 'some description,some fine cracks, small pebbles can kill,stairs front of school good lan',
     location: {
       lat: 39.8155,
       lng: -105.0726,
     },
+    features: {
+      curbs: {has: true, description: ['OK', 'getting old and chunky']},
+      flats: {has: true, description: ['Pristine', 'a whole skating rink']},
+      rails: {has: false, description: []},
+      stairs: {has: false, description: []},
+      security: {has: false, description: []},
+      public: true,
+      hazards: 'low'
+    }
   },
   {
     id: 3,
-    title: "Scott's butt",
+    title: "Scott's butt",    
+    description: '15 stair drop with good landing',
     location: {
       lat: 39.816,
       lng: -105.065,
     },
+    features: {
+      curbs: {has: false, description: []},
+      flats: {has: false, description: []},
+      rails: {has: false, description: []},
+      stairs: {has: true, description: ['Good', 'big 15 stairs with good runway and landing in lot']},
+      security: {has: false, description: []},
+      public: true,
+      hazards: 'high'
+    }
   },
   {
     id: 4,
     title: "Arvada SkatePark",
+    description: "Arvada's incredible skatepark",
     location: {
       lat: 39.82663239233444,
       lng: -105.14309411715244,
     },
+    features: {
+      curbs: {has: true, description: []},
+      flats: {has: true, description: []},
+      rails: {has: true, description: []},
+      stairs: {has: true, description: []},
+      security: {has: true, description: []},
+      public: true,
+      hazards: 'low'
+    }
   },
 ];
