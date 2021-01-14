f7b3fcb72c60adbe7e3c5582dcc12014fc895330
Scott Brabson <scottbrabson@gmail.com>
2021-01-13 21:30:05 -0700

    add mock data file

---
+++ src/sk8SpotData.js
@@ -0,0 +1,39 @@
+export const skateSpots = [
+  {
+    title: "Old church",
+    description: 'some description',
+    location: {
+      lat: 39.82597138963928, 
+      lng: -105.08527641669075
+    },
+    features: {
+      curbs: {has: true, description: ['pristine', 'some wax, recently poured']},
+      flats: {has: true, description: ['good', 'some fine cracks, small pebbles can kill']},
+      rails: {has: true, description: ['good', 'about 10ft long']},
+      stairs: {has: true, description: ['pristine', '10 stairs front of school good landing']},
+      security: {has: true, description: ['chill', 'guards were chill']},
+      public: false
+    }
+  },
+  {
+    title: "Secrest Park",
+    location: {
+      lat: 39.8155,
+      lng: -105.0726,
+    },
+  },
+  {
+    title: "Scott's butt",
+    location: {
+      lat: 39.816,
+      lng: -105.065,
+    },
+  },
+  {
+    title: "Arvada SkatePark",
+    location: {
+      lat: 39.82663239233444,
+      lng: -105.14309411715244,
+    },
+  },
+];
