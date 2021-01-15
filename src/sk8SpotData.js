dc98ecb2f6ca2df7519f5447aaab7ac275c623cf
Scott Brabson <scottbrabson@gmail.com>
2021-01-14 17:26:35 -0700

    update data structure

--- src/sk8SpotData.js
+++ src/sk8SpotData.js
@@ -7,15 +7,15 @@ export const skateSpots = [
       lat: 39.82597138963928, 
       lng: -105.08527641669075
     },
-    features: {
-      curbs: {has: true, description: ['pristine', 'some wax, recently poured']},
-      flats: {has: true, description: ['good', 'some fine cracks, small pebbles can kill']},
-      rails: {has: true, description: ['good', 'about 10ft long']},
-      stairs: {has: true, description: ['pristine', '10 stairs front of school good landing']},
-      security: {has: true, description: ['chill', 'guards were chill']},
-      public: false,
-      hazards: 'low'
-    }
+    features: [
+      {curbs: {has: true, condition: 5, description: 'some wax, recently poured'}},
+      {flats: {has: true, condition: 3, description: 'some fine cracks, small pebbles can kill'}},
+      {rails: {has: true, condition: 3, description: 'about 10ft long'}},
+      {stairs: {has: true, condition: 5, description: '10 stairs front of school good landing'}},
+      {security: {has: true, condition: 1, description: 'guards were chill'}},
+      {hazards: ['none']},
+      {public: false}
+    ]
   },
   {
     id: 2,
@@ -25,50 +25,50 @@ export const skateSpots = [
       lat: 39.8155,
       lng: -105.0726,
     },
-    features: {
-      curbs: {has: true, description: ['OK', 'getting old and chunky']},
-      flats: {has: true, description: ['Pristine', 'a whole skating rink']},
-      rails: {has: false, description: []},
-      stairs: {has: false, description: []},
-      security: {has: false, description: []},
-      public: true,
-      hazards: 'low'
-    }
+    features: [
+      {curbs: {has: true, condition: 2, description: 'getting old and chunky'}},
+      {flats: {has: true, condition: 5, description: 'a whole skating rink'}},
+      {rails: {has: false, condition: null, description: null}},
+      {stairs: {has: false, condition: null, description: null}},
+      {security: {has: false, condition: null, description: null}},
+      {hazards: ['small pebbles or chunks of pucks in the rink']},
+      {public: false}
+    ]
   },
   {
     id: 3,
-    title: "Scott's butt",    
+    title: "Big stair drop",    
     description: '15 stair drop with good landing',
     location: {
       lat: 39.816,
       lng: -105.065,
     },
-    features: {
-      curbs: {has: false, description: []},
-      flats: {has: false, description: []},
-      rails: {has: false, description: []},
-      stairs: {has: true, description: ['Good', 'big 15 stairs with good runway and landing in lot']},
-      security: {has: false, description: []},
-      public: true,
-      hazards: 'high'
-    }
+    features: [
+      {curbs: {has: false, condition: null, description: null}},
+      {flats: {has: false, condition: null, description: null}},
+      {rails: {has: false, condition: null, description: null}},
+      {stairs: {has: true, condition: 4, description: "big 15 stairs with good runway"}},
+      {security: {has: false, condition: null, description: null}},
+      {hazards: ['landing runs right into a busy street']},
+      {public: true}
+    ]
   },
   {
     id: 4,
     title: "Arvada SkatePark",
-    description: "Arvada's incredible skatepark",
+    description: "Colorado's best fakkin skatepark",
     location: {
       lat: 39.82663239233444,
       lng: -105.14309411715244,
     },
-    features: {
-      curbs: {has: true, description: []},
-      flats: {has: true, description: []},
-      rails: {has: true, description: []},
-      stairs: {has: true, description: []},
-      security: {has: true, description: []},
-      public: true,
-      hazards: 'low'
-    }
+    features: [
+      {curbs: {has: true, condition: 5, description: "it's all glorious"}},
+      {flats: {has: true, condition: 5, description: "it's all glorious"}},
+      {rails: {has: true, condition: 5, description: "it's all glorious"}},
+      {stairs: {has: true, condition: 4, description: "small drops all over"}},
+      {security: {has: false, condition: null, description: null}},
+      {hazards: ['scooter kids']},
+      {public: true}
+    ]
   },
 ];
