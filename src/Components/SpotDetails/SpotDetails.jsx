83efb1ecdfead03df52afa46e32d93ea9e16b456
Scott Brabson <scottbrabson@gmail.com>
2021-01-14 18:00:53 -0700

    Setup details view

--- src/Components/SpotDetails/SpotDetails.jsx
+++ src/Components/SpotDetails/SpotDetails.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React from "react";
 
-const SpotDetails = ({title, match}) => {
+const SpotDetails = ({ selectedSpot, match }) => {
+  const { id, title, description, location, features } = selectedSpot;
+
+  const featureList = (
+    <div className='feature-container'>
+      <h2>Features:</h2>
+      <div>
+        {features.map(f => (
+          <div className='feature-details' key={Date.now()}>
+            <h3>{`${f.type} ${f.has ? '✅': '❌'}`}</h3>
+            {f.has && <p>{f.description}</p>}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
   const onLoad = () => {
-    const id = match.params.movie_id;
+    // const id = match.params.spot_id;
     // if (+id !== movie_id) {
     //   syncMovieID(id);
     // }
-  }
+  };
 
-  return ( 
-    <div>
-      <h1>{title}</h1>
-    </div>
-   );
-}
- 
+  if (selectedSpot) {
+    return (
+      <div>
+        <h1>{title} is the spot!</h1>
+        <p>{description}</p>
+        <ul>{featureList}</ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Now where did I put that information...</h1>;
+      </div>
+    );
+  }
+};
+
 export default SpotDetails;
