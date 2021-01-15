c9f37a6433940f1d2f01152330c39ad1bfa5dfe1
Scott Brabson <scottbrabson@gmail.com>
2021-01-15 01:02:21 -0700

    Add styling to spot details

--- src/Components/SpotDetails/SpotDetails.jsx
+++ src/Components/SpotDetails/SpotDetails.jsx
@@ -1,28 +1,37 @@
 import React, { useEffect, useState } from "react";
 import {getLocal, saveLocal} from '../Common/Utilities/localStorage'
+import './SpotDetails.css'
 
 const SpotDetails = ({ selectedSpot, match }) => {
 
   const featureList = (
-    <div className="feature-container">
-      <h2>Features:</h2>
-      <div>
+    <div className="features-section">
+      {/* <h2>Features:</h2> */}
+      {/* <div> */}
         {selectedSpot?.features.map((f, i) => (
           <div className="feature-details" key={Date.now() + i}>
-            <h3>{`${f.type} ${f.has ? "✅" : "❌"}`}</h3>
+            <div className='feature-type'>
+              <h3>{`${f.type}`}</h3> 
+              <h3>{`${f.has ? "✅" : "❌"}`}</h3>
+            </div>
             {f.has && <p>{f.description}</p>}
           </div>
         ))}
-      </div>
+      {/* </div> */}
     </div>
   );
 
   if (selectedSpot) {
     return (
-      <div>
-        <h1>{selectedSpot.title}</h1>
-        <p>{selectedSpot.description}</p>
-        {featureList}
+      <div className='detail-view'>
+        <div className='detail-title-container'>
+          <h1>{selectedSpot.title}</h1>
+          <p>{selectedSpot.description}</p>
+          {<img alt='skating a curb' src={"https://3.bp.blogspot.com/-6wKDdxzO8AI/WBjzlwaP6dI/AAAAAAAABnY/Vh3Y4pTrQE0jh7WFpTpIjdULQhljrp9ywCEw/s1600/04%2BJoe.PNG"} />}
+        </div>
+        <div className='info-wrapper'>
+          {featureList}
+        </div>
       </div>
     );
   } else {
