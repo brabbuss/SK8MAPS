ebe9bce530f2bea5368d24b56395778cbbebae9b
Scott Brabson <scottbrabson@gmail.com>
2021-01-14 18:01:29 -0700

    update data structure

--- src/sk8SpotData.js
+++ src/sk8SpotData.js
@@ -2,56 +2,108 @@ export const skateSpots = [
   {
     id: 1,
     title: "Old church",
-    description: 'some description,some fine cracks, small pebbles can kill,stairs front of school good lan',
+    description:
+      "some description,some fine cracks, small pebbles can kill,stairs front of school good lan",
     location: {
-      lat: 39.82597138963928, 
-      lng: -105.08527641669075
+      lat: 39.82597138963928,
+      lng: -105.08527641669075,
     },
     features: [
-      {curbs: {has: true, condition: 5, description: 'some wax, recently poured'}},
-      {flats: {has: true, condition: 3, description: 'some fine cracks, small pebbles can kill'}},
-      {rails: {has: true, condition: 3, description: 'about 10ft long'}},
-      {stairs: {has: true, condition: 5, description: '10 stairs front of school good landing'}},
-      {security: {has: true, condition: 1, description: 'guards were chill'}},
-      {hazards: ['none']},
-      {public: false}
-    ]
+      {
+        type: "Curbs",
+        has: true,
+        condition: 5,
+        description: "some wax, recently poured",
+      },
+      {
+        type: "Flats",
+        has: true,
+        condition: 3,
+        description: "some fine cracks, small pebbles can kill",
+      },
+      {
+        type: "Rails",
+        has: true,
+        condition: 3,
+        description: "about 10ft long",
+      },
+      {
+        type: "Stairs",
+        has: true,
+        condition: 5,
+        description: "10 stairs front of school good landing",
+      },
+    ],
+    safety: {
+      security: {
+        has: true,
+        description: "guards were chill",
+      },
+      hazards: "some big cracks",
+      public: false,
+    },
   },
   {
     id: 2,
     title: "Secrest Park",
-    description: 'some description,some fine cracks, small pebbles can kill,stairs front of school good lan',
+    description:
+      "some description,some fine cracks, small pebbles can kill,stairs front of school good lan",
     location: {
       lat: 39.8155,
       lng: -105.0726,
     },
     features: [
-      {curbs: {has: true, condition: 2, description: 'getting old and chunky'}},
-      {flats: {has: true, condition: 5, description: 'a whole skating rink'}},
-      {rails: {has: false, condition: null, description: null}},
-      {stairs: {has: false, condition: null, description: null}},
-      {security: {has: false, condition: null, description: null}},
-      {hazards: ['small pebbles or chunks of pucks in the rink']},
-      {public: false}
-    ]
+      {
+        type: "Curbs",
+        has: true,
+        condition: 2,
+        description: "getting old and chunky",
+      },
+      {
+        type: "Flats",
+        has: true,
+        condition: 5,
+        description: "a whole skating rink",
+      },
+      { type: "Rails", has: false, condition: null, description: null },
+      { type: "Stairs", has: false, condition: null, description: null },
+    ],
+    safety: {
+      security: {
+        has: false,
+        description: null,
+      },
+      hazards: "small pebbles or chunks of pucks in the rink",
+      public: true,
+    },
   },
   {
     id: 3,
-    title: "Big stair drop",    
-    description: '15 stair drop with good landing',
+    title: "Big stair drop",
+    description: "15 stair drop with good landing",
     location: {
       lat: 39.816,
       lng: -105.065,
     },
     features: [
-      {curbs: {has: false, condition: null, description: null}},
-      {flats: {has: false, condition: null, description: null}},
-      {rails: {has: false, condition: null, description: null}},
-      {stairs: {has: true, condition: 4, description: "big 15 stairs with good runway"}},
-      {security: {has: false, condition: null, description: null}},
-      {hazards: ['landing runs right into a busy street']},
-      {public: true}
-    ]
+      { type: "Curbs", has: false, condition: null, description: null },
+      { type: "Flats", has: false, condition: null, description: null },
+      { type: "Rails", has: false, condition: null, description: null },
+      {
+        type: "Stairs",
+        has: true,
+        condition: 4,
+        description: "big 15 stairs with good runway",
+      },
+    ],
+    safety: {
+      security: {
+        has: false,
+        description: null,
+      },
+      hazards: "landing runs right into a busy street",
+      public: true,
+    },
   },
   {
     id: 4,
@@ -62,13 +114,38 @@ export const skateSpots = [
       lng: -105.14309411715244,
     },
     features: [
-      {curbs: {has: true, condition: 5, description: "it's all glorious"}},
-      {flats: {has: true, condition: 5, description: "it's all glorious"}},
-      {rails: {has: true, condition: 5, description: "it's all glorious"}},
-      {stairs: {has: true, condition: 4, description: "small drops all over"}},
-      {security: {has: false, condition: null, description: null}},
-      {hazards: ['scooter kids']},
-      {public: true}
-    ]
+      {
+        type: "Curbs",
+        has: true,
+        condition: 5,
+        description: "it's all glorious",
+      },
+      {
+        type: "Flats",
+        has: true,
+        condition: 5,
+        description: "it's all glorious",
+      },
+      {
+        type: "Rails",
+        has: true,
+        condition: 5,
+        description: "it's all glorious",
+      },
+      {
+        type: "Stairs",
+        has: true,
+        condition: 4,
+        description: "small drops all over",
+      },
+    ],
+    safety: {
+      security: {
+        has: false,
+        description: null,
+      },
+      hazards: "scooter kids",
+      public: true,
+    },
   },
 ];
