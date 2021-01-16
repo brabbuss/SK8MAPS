9853b38c39c999755ae59a3261e19d3de80c8e94
Scott Brabson <scottbrabson@gmail.com>
2021-01-15 20:48:51 -0700

    confirmation instantiates a new sk8map object

--- src/Components/Common/Map/ConfirmationMarker/ConfirmationMarker.jsx
+++ src/Components/Common/Map/ConfirmationMarker/ConfirmationMarker.jsx
@@ -7,23 +7,37 @@ import "./ConfirmationMarker.css";
 const ConfirmationMarker = () => {
   const [state, dispatch] = useContext(AppContext);
   
+  const handleClick = () => {
+    createSk8Map()
+    removeConfirmation()
+  }
+
   const removeConfirmation = () => {
-    const action = {type: 'ADD_NEW_SPOT_MARKER', newSpot: null}
+    const action = {type: 'ADD_CONFIRMATION_MARKER', marker: null}
     dispatch(action)
   }
+
+  const createSk8Map = () => {
+    const newSk8Map = {
+      id: Date.now()+1,
+      location: state.marker.location
+    }
+    const action = {type: 'CREATE_SK8MAP', newSk8Map: newSk8Map}
+    dispatch(action)
+  };
 
   const infoBox = (
     <div className="info-box">
       <p>Add a new SK8MAP here?</p>
       <Link to='/add/details' className="confirm-button">
-        <div onClick={removeConfirmation}>Confirm Placement</div>
+        <div onClick={handleClick}>Confirm Placement</div>
       </Link>
     </div>
   )
   
-  if (state.newSpot.location) {
+  if (state.marker.location) {
     return (
-      <InfoWindow position={state.newSpot.location} onCloseClick={removeConfirmation}>
+      <InfoWindow position={state.marker.location} onCloseClick={removeConfirmation}>
         {infoBox}
       </InfoWindow>
     );
